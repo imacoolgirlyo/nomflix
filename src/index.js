0ff5ed16efe76259e6def1833e8cd1b0ded2beae
imacoolgirlyo <tjrud1438@naver.com>
2018-07-26 03:26:17 +0900

    Day4 : Deploy gh-pages , Parcel

--- src/index.js
+++ src/index.js
@@ -1,16 +1,1 @@
-
-
-// const navigation = document.querySelector('nav');
-// const SCROLLED = "scrolled";
-
-
-// const navigationScroll = () => {
-//     if(window.scrollY > 400) {
-//         navigation.classList.add(SCROLLED);
-//     }else {
-//         navigation.classList.remove(SCROLLED);
-//     }
-    
-// }
-
-// window.addEventListener("scroll", navigationScroll);
+document.querySelector("body").innerHTML = 'hi';
